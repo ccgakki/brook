30ab545deb3ed022b8ca980b5fea60fa38309964
ccgakki <44969203+ccgakki@users.noreply.github.com>
2020-08-24 15:06:33 +0800

    Update brook-pf-mod.sh

--- brook-pf-mod.sh
+++ brook-pf-mod.sh
@@ -118,9 +118,9 @@ Download_brook(){
     [[ ! -e ${file} ]] && mkdir ${file}
     cd ${file}
     if [[ ${bit} == "x86_64" ]]; then
-        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook"
+        wget --no-check-certificate -N "wget http://103.150.8.165:8080/download/brook_linux_amd64"
     else
-        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_386"
+        wget --no-check-certificate -N "wget http://103.150.8.165:8080/download/brook_linux_386"
         mv brook_linux_386 brook
     fi
     [[ ! -e "brook" ]] && echo -e "${Error} Brook 下载失败 !" && exit 1
